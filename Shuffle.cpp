69bfe1075846eb381562cb601d5b7f976e8c727b
Damien Pernuit <dpernuit@gmail.com>
2016-07-29 22:51:13 -0400

    Cleaner, faster stronger... updated Shuffle.cpp
    - Added the option to ignore the output
    - Added the option to pause at the end (windows only)
    - Added the option to use a faster RNG than std::rand()
    - Cleaned the code / added comments

--- Shuffle.cpp
+++ Shuffle.cpp
@@ -1,32 +1,67 @@
 #include <iostream>		// for cin/cout
 #include <new>          // for std::bad_alloc
+#include <ctime>		// for the timer
 using namespace std;
+
+//
+//	Shuffle
+//
+//		Input a single integer N.
+//		Output the integers 1..N in random order.
+//
+//		Timers are already included in the program.
+//		More than 99% of the duration of the program is due to the output of 
+//		all the values to cout.
+//		For timing purposes, especially wheb using large values, you can disable
+//		the output to cout by setting bOutputAllValues to false.
+//
+//		The program automatically pauses at the end, using System("PAUSE"),
+//		This can be disabled by setting bPauseBeforeEnding to false.
+//
+//		fastrand is a faster alternative to std::rand()
+//		If you wish to use std::rand() instead, set bUseFastRand to false.
+//
+//		For extremely large values, ie: N = 1 Billion
+//		The program should be compiled in x64!
+//
+
+static bool bOutputAllValues = false;
+static bool bPauseBeforeEnding = false;
+static bool bUseFastRand = true;
+
+// bad cast, but will seed the fastrand fine
+static int g_nSeed = (int)std::time(NULL);
+
+// fastrand is a faster alternative to std::rand()
+inline int fastrand() 
+{ 
+  g_nSeed = (214013 * g_nSeed + 2531011); 
+  return (g_nSeed>>16)&0x7FFF; 
+} 
 
 int main() 
 {
+	// Input N from the user
     int nMAX;
     cin >> nMAX;
-    
+	cout << endl;
+
+	// Start the timer
+	std::clock_t timerStart = std::clock();
+        
     // Error case
     if(nMAX <= 0)
     {
         cout << "Invalid number, please input a number > 0" << endl;
         return 0;
     }        
-    
-    // Simplest case
-    if (nMAX == 1)
-    {
-        cout << "1" << endl;
-        return 0;
-    }
-    
+       
     // Building an array of all the values [1 .. N] 
 	try
 	{
 		// Using new to allocate memory on the heap
 		int* tValues = new int[nMAX];
-		
+
 		// Intializes the array with values from 1..N
 		for(int n = 0; n < nMAX; n++)
 		{
@@ -39,9 +74,13 @@ int main()
 		for(int i = nMAX-1; i > 0; i--)
 		{
 			// Returns a random number betwen 0 and i
-			nSwapIndex = std::rand() % (i+1);
+			if(bUseFastRand)
+				nSwapIndex = fastrand() % (i+1);
+			else
+				nSwapIndex = std::rand() % (i+1);
 			
-			// Extremely unlikely it happens => waste of time
+			
+			// It's extremely unlikely it'll happen => waste of time
 			// if (nSwapIndex == i)
 			//      continue;  
 			
@@ -51,22 +90,37 @@ int main()
 			tValues[i] = nTemp;
 		}
 		
-		// Output each value in a row
-		for(int n = 0; n < nMAX; n++)
-			cout << tValues[n] << endl;  
+		// Shuffle finished!
+		double dShuffleDuration = (std::clock() - timerStart);
+
+		if(bOutputAllValues)
+		{
+			// Output each value in a row
+			for(int n = 0; n < nMAX; n++)
+				cout << tValues[n] << endl;  
+		}
 		
 		// Clean the memory
 		delete[] tValues;
+
+		// Output the duration times
+		double dDuration = (std::clock() - timerStart);
+		cout << "Shuffled in " << dShuffleDuration << " ms." << endl;
+		cout << "Total time: " << dDuration << " ms." << endl;
 	}
 	catch (std::bad_alloc& ba)
 	{
 		// Not enough memory, please compile me in X64 ;)
-		cout << "Failed to allocate the array";   
+		cout << ba.what() << " Not enough memory to allocate the array" << endl;   
 	}
 	catch(...)
 	{
-		cout << "Exception occurred";
+		cout << "An exception occurred!" << endl; 
 	}
+
+	// Pause the program before returning (Windows only)
+	if(bPauseBeforeEnding)
+		system("PAUSE");
 
 	return 0;   
 }
